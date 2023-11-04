e55235202c98cf19299f6ec0bad2d1d6faacd796
Anas Mahmud <anasmahmud523@gmail.com>
2023-11-05 02:07:44 +0600

    SQL database connect successfully

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
+const { Pool } = require('pg');
 const app = require("./app");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
+
+const pool = new Pool({
+   host: "localhost",
+   port: 5432,
+   database: process.env.SQL_DATABASE,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_USER_PASS
+})
 
 async function main() {
    try {
+      await pool.connect();
+      console.log(`🛢️  database connection successfully`);
 
       app.listen(port, () => {
          console.log(`server listening on port ${port}`);
@@ -12,5 +23,6 @@ async function main() {
       console.log(`Failed to connect database`, error);
    }
 }
-
 main();
+
+module.exports = pool;
